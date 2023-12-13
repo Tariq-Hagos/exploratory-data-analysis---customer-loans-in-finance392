b01c1deca1ecfbc4a0caed892007d8cfe82a5ccd
tariq hagos <tariqhagos@gmail.com>
2023-12-13 14:45:49 +0000

    up to task 4

--- db_utils.py
+++ db_utils.py
@@ -1,10 +1,11 @@
-# database_connector.py
+#db_utils.py
 import yaml
 from sqlalchemy import create_engine
 import pandas as pd
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 """All the modules that will be used"""
 
@@ -83,19 +84,53 @@ class DataFrameInfo:
             'Null Percentage': null_percentage
         })
         return null_info
-
-
-class Plotter:
-    @staticmethod
-    def plot_null_distribution(df):
-        plt.figure(figsize=(12, 8))
-        sns.heatmap(df.isnull(), cmap='viridis', cbar=False, yticklabels=False)
-        plt.title("Null Values Distribution")
-        plt.show()
-
-
-class DataFrameTransform:
     
+    @staticmethod
+    def identify_and_visualize_skewed_columns(df, skewness_threshold=0.5):
+        numeric_columns = df.select_dtypes(include='number').columns
+        numeric_df = df[numeric_columns]
+        
+        if not numeric_df.empty:
+            skewed_columns = numeric_df.apply(lambda x: x.skew()).abs() > skewness_threshold
+            
+            # Use the original DataFrame columns to avoid indexing issues
+            selected_columns = [col for col in df.columns if col in numeric_columns and col in skewed_columns.index and skewed_columns[col]]
+            return selected_columns
+        else:
+            return []
+
+    @staticmethod
+    def identify_skewed_columns(df, skewness_threshold=0.5):
+        numeric_columns = df.select_dtypes(include='number').columns
+        numeric_df = df[numeric_columns]
+
+        if not numeric_df.empty:
+            skewed_columns = numeric_df.apply(lambda x: x.skew()).abs() > skewness_threshold
+            return numeric_df.columns[skewed_columns].tolist()
+        else:
+            return []
+
+
+class DataTransform:
+    
+    @staticmethod
+    def convert_to_numeric(df, numeric_columns):
+        try:
+            df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce')
+            return df
+        except Exception as e:
+            print(f"Error converting columns to numeric: {e}")
+            return None
+
+    @staticmethod
+    def convert_to_datetime(df, date_columns, date_format='%b-%Y'):
+        try:
+            df[date_columns] = df[date_columns].apply(pd.to_datetime, format=date_format, errors='coerce')
+            return df
+        except Exception as e:
+            print(f"Error converting columns to datetime: {e}")
+            return None
+        
     @staticmethod
     def drop_columns_with_nulls(df, threshold=0.5):
         null_percentage = df.isnull().mean()
@@ -109,25 +144,60 @@ class DataFrameTransform:
         imputer = SimpleImputer(strategy=strategy)
         df[numeric_columns] = imputer.fit_transform(df[numeric_columns])
         return df
-    
+   
     @staticmethod
-    def identify_skewed_columns(df, skewness_threshold=0.5):
-        skewed_columns = df.apply(lambda x: x.skew()).abs() > skewness_threshold
-        return df.columns[skewed_columns].tolist()
+    def transform_skewed_columns(df, skewed_columns):
+        for column in skewed_columns:
+            # Check if the column exists in the DataFrame
+            if column in df.columns:
+                df[column] = np.log1p(df[column])
+        return df
+
+class Plotter:
+    @staticmethod
+    def plot_null_distribution(df):
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(df.isnull(), cmap='viridis', cbar=False, yticklabels=False)
+        plt.title("Null Values Distribution")
+        plt.show()
+
+    @staticmethod
+    def visualize_skewness(df, skewed_columns):
+        plt.figure(figsize=(12, 8))
+        for column in skewed_columns:
+            sns.histplot(df[column], kde=True, label=column)
+        plt.title("Skewed Columns Distribution")
+        plt.legend()
+        plt.show()
 
 
-def identify_skewed_columns(df, skew_threshold=0.5):
-    """
-    Identify skewed columns in the DataFrame.
+class DataFrameTransform:
+    @staticmethod
+    def drop_columns_with_nulls(df, threshold=0.5):
+        null_percentage = df.isnull().mean()
+        columns_to_drop = null_percentage[null_percentage > threshold].index
+        df = df.drop(columns=columns_to_drop)
+        return df
 
-    Parameters:
-    - df: DataFrame
-    - skew_threshold: float, default=1.0
-      The threshold for considering a column as skewed.
+    @staticmethod
+    def impute_missing_values(df, strategy='median'):
+        numeric_columns = df.select_dtypes(include='number').columns
+        imputer = SimpleImputer(strategy=strategy)
+        df[numeric_columns] = imputer.fit_transform(df[numeric_columns])
+        return df
+   
+    def transform_skewed_columns(df, skewed_columns):
+        for column in skewed_columns:
+            df[column] = np.log1p(df[column])  
+        return df
 
-    Returns:
-    - List of column names identified as skewed.
-    """
-    skewed_columns = df.apply(lambda x: x.skew()).sort_values(ascending=False)
-    return skewed_columns[abs(skewed_columns) > skew_threshold].index.tolist()
+
+def identify_skewed_columns(df, skewness_threshold=0.5):
+    numeric_columns = df.select_dtypes(include='number').columns
+    numeric_df = df[numeric_columns]
+
+    skewed_columns = numeric_df.apply(lambda x: x.skew()).abs() > skewness_threshold
+    return numeric_df.columns[skewed_columns].tolist()
+
+
 
